697a4d35cb9b7e252b1c6ec7ea25b3fb31627f65
Drew <carullic@outlook.it>
2025-08-20 21:31:53 +0200

    Styling fixes and additional FAQ

--- src/app/[locale]/(main)/(furpanel)/room/_components/quotaViewer.tsx
+++ src/app/[locale]/(main)/(furpanel)/room/_components/quotaViewer.tsx
@@ -1,6 +1,7 @@
 import { useModalUpdate } from "@/components/context/modalProvider";
 import Button from "@/components/input/button";
 import ModalError from "@/components/modalError";
+import NoticeBox, { NoticeTheme } from "@/components/noticeBox";
 import { getRemainingRoomType, RoomStoreItemsApiAction, RoomStoreItemsApiResponse } from "@/lib/api/flows/roomOrderFlow";
 import { runRequest } from "@/lib/api/global";
 import { translate } from "@/lib/translations";
@@ -38,27 +39,36 @@ export default function QuotaViewer({ isOpen, modalLoading, setModalLoading, clo
     }, [roomsData, isOpen]);
 
     return <>
-        <div className="horizontal-list flex-vertical-center">
-            <span className="title">{t("furpanel.room.quota_viewer.available_types")}</span>
+        <div className="horizontal-list gap-4mm flex-vertical-center">
+            <NoticeBox theme={NoticeTheme.FAQ}>
+                {t("furpanel.room.quota_viewer.description")}
+            </NoticeBox>
             <div className="spacer"></div>
-            <Button iconName={"REFRESH"}
-                onClick={() => setRoomsData(null)}
-                debounce={3000}>
-                {t("common.reload")}
-            </Button>
+            <div>
+                <Button iconName={"REFRESH"}
+                    onClick={() => setRoomsData(null)}
+                    debounce={3000}
+                    busy={modalLoading}>
+                    {t("common.reload")}
+                </Button>
+            </div>
+
         </div>
-        <div className="vertical-list gap-4mm">
-            {roomsData?.rooms?.map((roomInfo, index) =>
-                <a className="room-type-container horizontal-list gap-2mm flex-vertical-center rounded-m"
-                    key={index}>
-                    <div className="vertical-list">
-                        <span className="title">{translate(roomInfo.data.roomTypeNames, locale)}</span>
-                        <span className="descriptive color-subtitle">
-                            {t("furpanel.room.order_flow.quota_left", { size: getRemainingRoomType(roomInfo) })}
-                        </span>
-                    </div>
-                </a>
-            )}
+        <div style={{ paddingLeft: '1em' }}>
+            <ul className="vertical-list gap-4mm">
+                {roomsData?.rooms?.map((roomInfo, index) =>
+                    <li key={index}>
+                        <a className="room-type-container horizontal-list gap-2mm flex-vertical-center rounded-m">
+                            <div className="vertical-list">
+                                <span className="title">{translate(roomInfo.data.roomTypeNames, locale)}</span>
+                                <span className="descriptive color-subtitle">
+                                    {t("furpanel.room.order_flow.quota_left", { size: getRemainingRoomType(roomInfo) })}
+                                </span>
+                            </div>
+                        </a>
+                    </li>
+                )}
+            </ul>
         </div>
     </>
 }
