b064e0e11f7ff9600e8e17bb82fbba93256f3674
Andrea Carulli <38762811+Blastd@users.noreply.github.com>
2026-01-05 20:07:27 +0100

    Bugfix/106-1-middleware-fixes (#112)
    * Moved back to translation folder
    
    * Routing fixes

--- src/app/[locale]/(authentication)/logging/page.tsx
+++ src/app/[locale]/(authentication)/logging/page.tsx
@@ -2,26 +2,27 @@
 import Icon from "@/components/icon";
 import { useUser } from "@/components/context/userProvider";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Login() {
+export default function Logging() {
     const t = useTranslations("authentication");
     const router = useRouter();
-    const { setUserDisplay } = useUser();
+    const { setUpdateUser } = useUser();
+    const params = useSearchParams();
+
     useEffect(() => {
-        setUserDisplay(undefined);
-        router.replace("/login");
-        router.refresh();
+        setUpdateUser(true);
+        router.replace(params.get("continue") ?? "/home");
     }, [])
 
     return <>
         <div className="horizontal-list gap-4mm flex-center">
             <span className="title-pair">
-                <Icon icon="DESIGN_SERVICES"/>
+                <Icon icon="DESIGN_SERVICES" />
                 <span className="titular bold highlight">furpanel</span>
                 <span> - </span>
-                <span className="titular bold">{t('logout.title').toLowerCase()}</span>
+                <span className="titular bold">{t('logging_in.title').toLowerCase()}</span>
             </span>
         </div>
     </>;
