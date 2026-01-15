4f6108e81f6a117b1bf78e8c9cb557cd4e01a022
Blastd <rctandrew100@gmail.com>
2026-01-15 16:40:09 +0100

    user dropdown popover WIP

--- src/components/userDropdown.tsx
+++ src/components/userDropdown.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useLocale, useTranslations } from 'next-intl';
 import { Link, routing, useRouter } from '@/i18n/routing';
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, ToggleEvent, useId, useState } from 'react';
 import Icon from '@/components/icon';
 import UserPicture from '@/components/userPicture';
 import { runRequest } from '@/lib/api/global';
@@ -13,11 +13,11 @@ import "@/styles/components/userDropDown.css";
 import { mapLanguageToFlag } from '@/lib/utils';
 
 export default function UserDropDown({ userData, loading }: Readonly<{ userData?: UserData, loading: boolean }>) {
-    const [isOpen, setOpen] = useState(false);
-    const [isHover, setHover] = useState(false);
     const t = useTranslations('common');
     const router = useRouter();
     const locale = useLocale();
+    const [isOpen, setOpen] = useState(false);
+    const id = useId();
 
     const logout = () => {
         runRequest(new LogoutApiAction())
@@ -25,21 +25,20 @@ export default function UserDropDown({ userData, loading }: Readonly<{ userData?
             .finally(() => router.replace("/logout"));
     }
 
-    const optionClick = (e: MouseEvent<HTMLDivElement>) => {
-        setOpen(!isOpen);
-        e.stopPropagation();
-    }
-
     const loginClick = (e: MouseEvent<HTMLButtonElement>) => {
         e.stopPropagation();
         router.push('/login');
     }
 
+    const onToggle = (e: ToggleEvent<HTMLDivElement>) => {
+        setOpen (e.newState === "open");
+    }
+
     return (
         <div className="user-dropdown rounded-m">
-            <div tabIndex={0} className="dropdown-button horizontal-list flex-vertical-center gap-2mm rounded-m"
-                onClick={() => setOpen(!isOpen)} onBlur={() => { if (!isHover) setOpen(false) }}
-                onPointerOver={() => setHover(true)} onPointerLeave={() => setHover(false)}>
+            <button tabIndex={0}
+                className="dropdown-button horizontal-list flex-vertical-center gap-2mm rounded-m"
+                popoverTarget={id}>
                 {loading && <LoadingPanel />}
                 {!userData && !loading && <Button onClick={loginClick} iconName={"KEY"}>
                     {t('header.login')}
@@ -49,17 +48,23 @@ export default function UserDropDown({ userData, loading }: Readonly<{ userData?
                     <span className="title average semibold nickname">{userData.fursonaName}</span>
                 </>}
                 <Icon style={{ fontSize: "24px" }} icon={(isOpen) ? "ARROW_DROP_UP" : "ARROW_DROP_DOWN"}/>
-            </div>
-            <div className={`vertical-list dropdown-container rounded-m ${(isOpen) && 'open'}`} onClick={optionClick}
-                onPointerOver={() => setHover(true)} onPointerLeave={() => setHover(false)}>
+            </button>
+            <div id={id}
+                className={`vertical-list dropdown-container rounded-m ${isOpen ? "open" : ""}`}
+                popover="auto"
+                onToggle={onToggle}>
                 {/* Logout */}
-                {userData && <a href='#' onClick={() => logout()} className='title small rounded-s vertical-align-middle'>
+                {userData && <a href="#"
+                    onClick={() => logout()}
+                    className="title small rounded-s vertical-align-middle">
                     {t('header.dropdown.logout')}
                 </a>}
                 {/* Language selector */}
                 <hr />
-                {routing.locales.map((lng, index) => <Link href="#" className='title small rounded-s vertical-align-middle horizontal-list'
-                    key={index} onClick={() => changeLanguage(lng, userData)}>
+                {routing.locales.filter (lng => lng !== locale).map((lng, index) => <Link href="#"
+                    className="title small rounded-s vertical-align-middle horizontal-list"
+                    key={index}
+                    onClick={() => changeLanguage(lng, userData)}>
                     {mapLanguageToFlag(lng)}&nbsp;
                     {t(`header.dropdown.language.${lng}`)}
                     {lng === locale && <Icon className='medium' icon="CHECK"/>}
