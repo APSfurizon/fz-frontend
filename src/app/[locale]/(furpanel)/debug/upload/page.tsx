633c222adad5fb727a21bca44e684f9806a3fe1a
Blastd <rctandrew100@gmail.com>
2026-04-07 12:46:39 +0200

    Upload md5 completion fix

--- src/app/[locale]/(furpanel)/debug/upload/page.tsx
+++ src/app/[locale]/(furpanel)/debug/upload/page.tsx
@@ -1,5 +1,21 @@
 "use client"
+
+import { useUser } from "@/components/context/userProvider"
+import { upload } from "@/lib/api/gallery/upload";
+import { ChangeEvent } from "react";
+
 export default function DebugUpload() {
+    const { userDisplay } = useUser();
+    const runUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.length ? e.target.files[0] : null;
+        if (file && userDisplay) {
+            upload(file, userDisplay.display.userId)
+                .then(e => console.log("SUCCESS", e))
+                .catch(e => console.error("FAIL", e));
+        }
+    }
+
     return <div className="page">
+        <input type="file" onChange={runUpload} />
     </div>
 }
--- src/lib/api/gallery/upload.ts
+++ src/lib/api/gallery/upload.ts
@@ -1,4 +1,5 @@
 import { ApiAction, ApiErrorResponse, ApiResponse, RequestType, runRequest } from "../global";
+import CryptoJS from "crypto-js";
 
 export type GalleryUploadApiBody = {
     fileSize: number;
@@ -27,7 +28,24 @@ export interface GalleryUploadApiResponse extends ApiResponse {
 export class GalleryUploadApiAction extends ApiAction<GalleryUploadApiResponse, ApiErrorResponse> {
     authenticated = true;
     method = RequestType.POST;
-    urlAction = "gallery/upload";
+    urlAction = "gallery/upload/";
+}
+
+export type GalleryUploadCompleteApiBody = {
+    fileName: string;
+    uploadReqId: number;
+    fileSize: number;
+    eventId: number;
+    uploadRepostPermissions: "PHOTOGRAPHER_DISCRETION",
+    etags: string[];
+    md5Hash: string;
+    userId: number;
+}
+
+export class GalleryUploadCompleteApiAction extends ApiAction<ApiResponse, ApiErrorResponse> {
+    authenticated = true;
+    method = RequestType.POST;
+    urlAction = "gallery/upload/complete";
 }
 
 /**
@@ -39,11 +57,23 @@ export type ChunkUpload = {
     offsetStart: number;
     link: string;
     etag: string | null;
-    md5Checksum: string | null;
+    md5Checksum: CryptoJS.lib.WordArray | null;
     tries: number;
 }
 
-function upload(file: File, userId: number) {
+const ChunkUploadFailTypes = {
+    REQUEST_FAILED: "request_failed",
+    NETWORK_ERROR: "network_error",
+
+} as const;
+export type ChunkUploadFailType = typeof ChunkUploadFailTypes[keyof typeof ChunkUploadFailTypes];
+
+export type ChunkUploadFail = {
+    chunkData: ChunkUpload;
+    error: ChunkUploadFailType
+}
+
+export function upload(file: File, userId: number) {
     return new Promise(async (resolve, reject) => {
         try {
             // Create upload links
@@ -61,7 +91,14 @@ function upload(file: File, userId: number) {
                 });
             });
             // Upload chunks
-            uploadAllChunks(file, uploadRequestData.multipartCreationResponse.chunkSize, chunks);
+            const uploadedChunks = await uploadAllChunks(file,
+                uploadRequestData.multipartCreationResponse.chunkSize, chunks);
+            // Confirm upload
+            const etags = uploadedChunks.map(c => c.etag ?? "");
+            const concatHashes = uploadedChunks.map(c => c.md5Checksum).filter(v => !!v)
+                .reduce((last, newValue) => last!.concat(newValue), CryptoJS.lib.WordArray.create());
+            const md5Hash = CryptoJS.MD5(concatHashes).toString(CryptoJS.enc.Hex);
+            resolve(await completeUpload(file, userId, uploadRequestData.uploadReqId, etags, md5Hash));
         } catch (e) {
             reject(e);
         }
@@ -71,7 +108,7 @@ function upload(file: File, userId: number) {
 
 function beginUpload(file: File, userId: number) {
     const uploadApiBody: GalleryUploadApiBody = {
-        eventId: 2,
+        eventId: 1,
         fileName: file.name,
         fileSize: file.size,
         userId
@@ -83,16 +120,17 @@ function beginUpload(file: File, userId: number) {
     });
 }
 
-function uploadAllChunks(file: File, chunkSize: number, chunks: ChunkUpload[]) {
+function uploadAllChunks(file: File, chunkSize: number, chunks: ChunkUpload[]): Promise<ChunkUpload[]> {
     const MAX_RUNNING_WORKERS = 3;
     const batchedChunks: ChunkUpload[][] = [];
     slidingWindow(chunks, MAX_RUNNING_WORKERS, (data) => batchedChunks.push(data));
+    console.log("Batches", batchedChunks);
     return new Promise(async (resolve, reject) => {
-        const toReturn = [];
+        const toReturn: ChunkUpload[] = [];
         try {
             for (const toUpload of batchedChunks) {
                 const executed = await Promise.all(toUpload.map(chunk => uploadChunk(file, chunkSize, chunk)));
-                toReturn.push(executed);
+                toReturn.push(...executed);
             }
         } catch (e) { reject(e); }
         resolve(toReturn);
@@ -103,19 +141,69 @@ function uploadChunk(file: File, chunkSize: number, chunkData: ChunkUpload): Pro
     const MAX_TRIES = 3;
     return new Promise(async (resolve, reject) => {
         const data = { ...chunkData };
+        const slicedFile = file.slice(data.offsetStart, data.offsetStart + chunkSize);
+        // Create md5 checksum of chunk
+        const sliceArray = await slicedFile.arrayBuffer();
+        const wordArray = CryptoJS.lib.WordArray.create(sliceArray);
+        data.md5Checksum = CryptoJS.MD5(wordArray);
+        console.debug(`Chunk ${data.link} - Md5 = ${data.md5Checksum}`);
+        let lastError: any = null;
         while (!data.success && data.tries <= MAX_TRIES) {
-            const request = new XMLHttpRequest();
-            request.open("PUT", data.link);
             try {
-                request.send(file.slice(data.offsetStart, data.offsetStart + chunkSize));
+                const toReturn = await runChunkUpload(slicedFile, data);
+                data.success = true;
+                resolve(toReturn);
+                break;
             } catch (e) {
-
+                lastError = e;
+                data.tries += 1;
+                console.warn(`Upload of chunk to ${data.link} failed:`, e);
             }
         }
         if (data.tries > MAX_TRIES) {
-
+            reject({ chunkData: data, error: lastError });
         }
     });
+}
+
+function runChunkUpload(slicedFile: Blob, chunkData: ChunkUpload): Promise<ChunkUpload> {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open("PUT", chunkData.link);
+        request.onload = () => {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if ([200, 201, 202, 203, 204, 207, 208].includes(request.status)) {
+                    // Retrieve chunk etag
+                    chunkData.etag = request.getResponseHeader("etag")?.replaceAll("\"", "") ?? null;
+                    console.log(`Chunk ${chunkData.link} SUCCESS. Etag: ${chunkData.etag}`)
+                    resolve(chunkData);
+                } else {
+                    reject(ChunkUploadFailTypes.REQUEST_FAILED);
+                }
+            }
+        }
+        request.onerror = () => {
+            reject(reject(ChunkUploadFailTypes.NETWORK_ERROR));
+        }
+        request.send(slicedFile);
+    })
+}
+
+function completeUpload(file: File, userId: number, uploadReqId: number, etags: string[], md5Hash: string) {
+    const body: GalleryUploadCompleteApiBody = {
+        fileName: file.name,
+        uploadReqId,
+        fileSize: file.size,
+        eventId: 1,
+        uploadRepostPermissions: "PHOTOGRAPHER_DISCRETION",
+        etags,
+        md5Hash,
+        userId
+    }
+    return runRequest({
+        action: new GalleryUploadCompleteApiAction(),
+        body
+    })
 }
 
 function slidingWindow<T, U>(data: T[], windowSize: number, onWindow: (data: T[]) => U) {
