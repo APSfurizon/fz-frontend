bf5fe59e612e835d8a6a54a5750a6c311a7ab40d
Blastd <rctandrew100@gmail.com>
2026-08-02 15:41:15 +0200

    Added some components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { ButtonGroup, ButtonGroupSeparator, ButtonGroupText, buttonGroupVariants } from "@/components/ui/button-group";
+import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "../ui/accordion";
+import { Badge } from "../ui/badge";
 
-export { Button };
+export {
+  Accordion,
+  Badge,
+  Button,
+  ButtonGroup,
+  ButtonGroupSeparator,
+  ButtonGroupText,
+  buttonGroupVariants,
+  Card,
+  Skeleton,
+};
